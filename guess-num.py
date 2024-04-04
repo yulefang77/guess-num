8b9c68bb3ce44d2f29f54e7a7c7e545175b7fcad
yule <yulefang77@gmail.com>
2024-04-04 13:06:52 +0800

    'add infromation of counter

--- guess-num.py
+++ guess-num.py
@@ -1,16 +1,20 @@
 # 猜數字程式 數字範圍 1-100
 # 猜對印出 “你猜對了！“
 # 猜錯印出 “你猜錯了！你的數字比答案大/小。加油！再猜一次！“
+# 每次猜完後列印出 “你猜了 n 次了喔！“
 
 import random
 
 r = random.randint(1, 100)
+count = 0
 
 while True:
-	guess = input('請猜一個數字')
+	count += 1
+	guess = input('請猜一個數字: ')
 	guess = int(guess)
 	if r == guess:
 		print('你猜對了！')
+		print('你一共猜了 ', count, ' 次。')
 		break
 	elif r > guess:
 		print('你猜錯了！')
@@ -18,3 +22,4 @@ while True:
 	elif r < guess:
 		print('你猜錯了！')
 		print('你的數字比答案大。加油！再猜一次！')
+	print('你猜了 ', count, ' 次了喔！')
